aaacca1915420a2d1cf9e6dc395fa2e3e9800ba3
hljubic <hrvoje.ljubic@gmail.com>
2024-09-04 21:52:29 +0200

    Novi file

--- SOFTS/models/SOFTS.py
+++ SOFTS/models/SOFTS.py
@@ -86,7 +86,7 @@ class STAR(nn.Module):
         self.gen3 = nn.Linear(d_series + d_core, d_series)
         self.gen4 = nn.Linear(d_series, d_series)
         
-        self.activation = LearnableAsymCauchy()
+        self.activation = nn.SiLU()#LearnableAsymCauchy()
 
     def forward(self, input, *args, **kwargs):
         batch_size, channels, d_series = input.shape
